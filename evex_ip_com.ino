5d69b3898a17d6c8bc8497c2ea1700c43812e859
lautaronahuelp <lautaro@outlook.com>
2024-07-27 21:30:09 -0300

    Inicio del proyecto

---
+++ evex_ip_com.ino
@@ -0,0 +1,14 @@
+#include <dscKeybusInterface.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <cppQueue.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
